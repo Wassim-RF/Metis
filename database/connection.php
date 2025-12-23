1d991222f7a0dde73c6129654636656791794d40
Wassim-RF <wassimrifi0313@gmail.com>
2025-12-23 22:35:05 +0100

    J'ai creer le code de connection avec la database tel j'ai le fait comme une classe c'est propreitise c'est les info du db et une seule methode qui est la fct du link

--- database/connection.php
+++ database/connection.php
@@ -1,11 +1,20 @@
 <?php
-    try {
-        $dsn = "mysql:host=127.0.0.1;dbname=metis;port=3306;charset=utf8";
-        $pdo = new PDO($dsn, 'root', 1980);
 
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    class DataBase {
+        private string $db_host = "127.0.0.1";
+        private string $db_name = "metis";
+        private int $db_port = 3306;
+        private string $db_user = "root";
+        private int $db_pass = 1980;
+        private ?PDO $pdo = null;
 
-    } catch (PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
+        public function connect() {
+            if ($this->pdo === null) {
+                $dsn = "mysql:host=$this->db_host;dbname=$this->db_name;port=$this->db_port;charset=utf8";
+                $this->pdo = new PDO($dsn, $this->db_user, $this->db_pass);
+                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            }
+            return $this->connect();
+        }
     }
